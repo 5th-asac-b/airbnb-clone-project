982407866ace12b07533edc7cd79d80a74bb534e
quswjdgns399 <quswjdgns3996@gmail.com>
2024-05-21 19:42:05 +0900

    feat(components): add DetailGallery component
    Added DetailGallery component to display main and sub images using data from imageData.json. Implemented responsive layout with Tailwind CSS.

---
+++ src/components/DetailGallery.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import imageData from '../assets/imageData.json'
+
+const DetailGallery = () => {
+  return (
+    <div className='p-2'>
+      <div className='flex gap-2'>
+        <div className='flex-1'>
+          <div className='h-full'>
+            <img
+              src={imageData.mainImage}
+              alt='Main'
+              className='w-full h-full object-cover rounded-l-lg'
+            />
+          </div>
+        </div>
+        <div className='flex-1 grid grid-cols-2 gap-2'>
+          {imageData.subImages.map((src, index) => (
+            <div key={index} className='relative'>
+              <img
+                src={src}
+                alt={`Sub ${index + 1}`}
+                className={`absolute inset-0 w-full h-full object-cover ${index === 1 ? 'rounded-tr-lg' : ''} ${index === 3 ? 'rounded-br-lg' : ''}`}
+              />
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default DetailGallery
