4b1e570d8c1addac05b122f9f806785fd55a0e48
Dradradre <leeyonghak13@gmail.com>
2024-05-22 23:05:45 +0900

    Feat(tailwind) : 사용자 지정 색상 #F0EFE9 추가
    호스트 섹션에 필요한 background color config 파일에 추가하였음

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,9 @@ export default {
       fontFamily: {
         'sf-pro': ['"SF Pro"', 'sans-serif'],
       },
+      colors: {
+        hostSectionBackground: '#F0EFE9',
+      },
     },
   },
   plugins: [
